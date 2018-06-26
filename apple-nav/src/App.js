904b8f9b24b8d6702325faafcc6276fea484e762
Alec <afroenygam@gmail.com>
2018-06-26 18:23:11 -0400

    create-react-app installed, environment configured

---
+++ apple-nav/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {Route} from 'react-router-dom';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
